007cec70e13ddea4d7f25f934e677f2e192f8a6f
Viesich <v.s.vesich@gmail.com>
2024-10-07 16:49:58 +0300

    add serialize_car_object & deserialize_car_object function in main

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import json
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return json.dumps(serializer.data).encode("utf-8")
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_bytes: bytes) -> Car:
+    json_str = json_bytes.decode("utf-8")
+    data = json.loads(json_str)
+
+    serializer = CarSerializer(data=data)
+
+    if serializer.is_valid(raise_exception=True):
+        car_instance = serializer.save()
+        return car_instance
